e734b8b3f14b007f476e18b547abd1cba2cc155a
Conor O'Sullivan <conorosully1@gmail.com>
2020-04-24 23:10:26 +0100

    Added file that calculates IV

---
+++ src/information_value.py
@@ -0,0 +1,49 @@
+# Calculate information value
+import pandas as pd
+import numpy as np
+
+def calc_iv(df, feature, target, pr=False):
+    """
+    Set pr=True to enable printing of output.
+
+    Output:
+      * iv: float,
+      * data: pandas.DataFrame
+    """
+
+    lst = []
+
+    df[feature] = df[feature].fillna("NULL")
+
+    for i in range(df[feature].nunique()):
+        val = list(df[feature].unique())[i]
+        lst.append([feature,                                                        # Variable
+                    val,                                                            # Value
+                    df[df[feature] == val].count()[feature],                        # All
+                    df[(df[feature] == val) & (df[target] == 0)].count()[feature],  # Good (think: Fraud == 0)
+                    df[(df[feature] == val) & (df[target] == 1)].count()[feature]]) # Bad (think: Fraud == 1)
+
+    data = pd.DataFrame(lst, columns=['Variable', 'Value', 'All', 'Good', 'Bad'])
+
+    data['Share'] = data['All'] / data['All'].sum()
+    data['Bad Rate'] = data['Bad'] / data['All']
+    data['Distribution Good'] = (data['All'] - data['Bad']) / (data['All'].sum() - data['Bad'].sum())
+    data['Distribution Bad'] = data['Bad'] / data['Bad'].sum()
+    data['WoE'] = np.log(data['Distribution Good'] / data['Distribution Bad'])
+
+    data = data.replace({'WoE': {np.inf: 0, -np.inf: 0}})
+
+    data['IV'] = data['WoE'] * (data['Distribution Good'] - data['Distribution Bad'])
+
+    data = data.sort_values(by=['Variable', 'Value'], ascending=[True, True])
+    data.index = range(len(data.index))
+
+    if pr:
+        print(data)
+        print('IV = ', data['IV'].sum())
+
+
+    iv = data['IV'].sum()
+    # print(iv)
+
+    return iv
